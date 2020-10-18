d2eb3b505d1a2d8bb44ec1ed2e27c558171119bc
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-10-18 12:09:44 +0100

    Create studentas.h

---
+++ studentas.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef STUDENTAS_H_INCLUDED
+#define STUDENTAS_H_INCLUDED
+
+#include "funkcijos.h"
+
+struct studentas
+{
+    string vardai;
+    string pavardes;
+    vector<int> pazymiai;
+};
+
+#endif

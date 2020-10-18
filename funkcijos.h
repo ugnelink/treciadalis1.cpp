23fb23d6d761c384aad1fe31e670489f4e9b87f6
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-10-18 12:01:04 +0100

    Create funkcijos.h

---
+++ funkcijos.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cstdio>
+#include <sstream> 
+
+#endif

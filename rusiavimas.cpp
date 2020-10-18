79cfebbe07c434117a873548243933eab5285823
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-10-18 12:01:28 +0100

    Create rusiavimas.cpp

---
+++ rusiavimas.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef RUSIAVIMAS_H_INCLUDED
+#define RUSIAVIMAS_H_INCLUDED
+
+#include "funkcijos.h"
+
+struct rusiavimas
+{
+    string vardai;
+    string pavardes;
+    double vidurkiai;
+    double medianos;
+};
+
+#endif

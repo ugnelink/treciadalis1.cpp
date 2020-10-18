9e9cae0e13425268168f67c1b20166d79a55723e
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-10-18 11:59:50 +0100

    Create duomenys.h

---
+++ duomenys.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DUOMENYS_H_INCLUDED
+#define DUOMENYS_H_INCLUDED
+
+#include "funkcijos.h"
+
+struct duomenys
+{
+    string vardai;
+    string pavardes;
+    vector<string> pazymiai;
+};
+
+#endif
